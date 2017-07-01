0a98ce894c62bd89a67744f85f29cb55bba08a2f
Pavel Ivanov <tyrension@gmail.com>
2017-07-01 17:43:22 +0700

    Add User model and migration.

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+/**
+ * Class User
+ * @package App\Models
+ * @property int id;
+ * @property string username;
+ * @property string banner;
+ * @property string avatar;
+ * @property Carbon created_at;
+ * @property Carbon updated_at;
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'banner', 'avatar',
+    ];
+}
