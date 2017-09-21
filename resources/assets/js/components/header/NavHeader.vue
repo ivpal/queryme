237ffaa75b277a981a72f86310ea14c90d12382c
Pavel Ivanov <p.ivanov@vsemayki.ru>
2017-09-21 09:16:40 +0700

    Remove bootstrap.

--- resources/assets/js/components/header/NavHeader.vue
+++ resources/assets/js/components/header/NavHeader.vue
@@ -1,20 +1,26 @@
 <template>
-  <nav class="navbar navbar-toggleable-md header">
-    <div class="container">
-      <a class="navbar-brand brand-link" href="/">Queryme</a>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="/discover">Discover</a>
-          </li>
-        </ul>
-        <div class="my-2 my-md-0">
-          <span class="login" v-if="canShowLogin()" @click="showLoginWindow">Вход</span>
-          <user-actions></user-actions>
-        </div>
+  <div class="main-header-box">
+    <div class="main-header">
+      <div class="container">
+        <a href="#" class="logo">Queryme</a>
+
+        <nav class="main-nav">
+          <ul class="nav-list">
+            <div class="main-nav-list">
+              <li class="nav-item link-item">
+                <a href="#">Популярное</a>
+              </li>
+            </div>
+            <li class="nav-item auth">
+              <span class="login">
+                Вход
+              </span>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -44,40 +50,76 @@ export default {
 <style lang="scss">
 @import "../../../sass/variables";
 
-.header {
-  .nav-link {
-    color: $nav-item-color;
-    line-height: 3rem;
+.main-header-box {
+  position: relative;
+}
 
-    &:hover {
-      color: $brand-primary;
-    }
+.main-header {
+  position: fixed;
+  background: #fff;
+  color: #909090;
+  height: $header-height;
+  z-index: 250;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: all .2s;
+  border-bottom: 1px solid #f1f1f1;
+
+  .container {
+    height: 100%;
+    display: flex;
+    align-items: center;
   }
 }
 
-.logout {
+.logo {
+  font-size: 1.6rem;
+  margin-right: 2rem;
   color: $brand-primary;
-  line-height: 3.9rem;
-
-  &:hover {
-    color: $brand-primary-hover;
-  }
 }
 
-.brand-link {
-  &:hover {
-    color: $brand-primary;
-  }
+.main-nav {
+  height: 100%;
+  flex: 1 0 auto;
 }
 
-.login {
-  color: $brand-primary;
+.nav-list {
+  margin: 0;
+  height: 100%;
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.main-nav-list {
+  display: flex;
+}
+  
+.nav-item {
+  margin: 0;
+  height: 100%;
+  display: flex;
+  color: $nav-item-color;
   cursor: pointer;
-  line-height: 4rem;
-  vertical-align: middle;
+  padding: 0 1.5rem;
+  font-size: 1.2rem;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+}
+
+.link-item a:hover {
+  color: $nav-active-color;
+}
+
+.auth {
+  color: $nav-active-color;
 
   &:hover {
-    color: $brand-primary-hover;
+    color: $nav-auth-hover-color;
   }
 }
+
 </style>
